5bc5dda01086973aaf1198b65842a8bc1444cdff
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-06 12:51:38 -0500

    Add project files.

---
+++ Video_141/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtFrom = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtTo = New System.Windows.Forms.TextBox()
+        Me.txtSubject = New System.Windows.Forms.TextBox()
+        Me.txtBody = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.btnResetFields = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "From:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(149, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(22, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "To:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 108)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Subject:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 152)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Body:"
+        '
+        'txtFrom
+        '
+        Me.txtFrom.Location = New System.Drawing.Point(12, 27)
+        Me.txtFrom.Name = "txtFrom"
+        Me.txtFrom.Size = New System.Drawing.Size(100, 23)
+        Me.txtFrom.TabIndex = 5
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(149, 27)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 23)
+        Me.txtPassword.TabIndex = 6
+        '
+        'txtTo
+        '
+        Me.txtTo.Location = New System.Drawing.Point(12, 82)
+        Me.txtTo.Name = "txtTo"
+        Me.txtTo.Size = New System.Drawing.Size(237, 23)
+        Me.txtTo.TabIndex = 7
+        '
+        'txtSubject
+        '
+        Me.txtSubject.Location = New System.Drawing.Point(12, 126)
+        Me.txtSubject.Name = "txtSubject"
+        Me.txtSubject.Size = New System.Drawing.Size(237, 23)
+        Me.txtSubject.TabIndex = 8
+        '
+        'txtBody
+        '
+        Me.txtBody.Location = New System.Drawing.Point(12, 170)
+        Me.txtBody.Multiline = True
+        Me.txtBody.Name = "txtBody"
+        Me.txtBody.Size = New System.Drawing.Size(237, 92)
+        Me.txtBody.TabIndex = 9
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(12, 269)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 10
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'btnResetFields
+        '
+        Me.btnResetFields.Location = New System.Drawing.Point(149, 269)
+        Me.btnResetFields.Name = "btnResetFields"
+        Me.btnResetFields.Size = New System.Drawing.Size(75, 23)
+        Me.btnResetFields.TabIndex = 11
+        Me.btnResetFields.Text = "Discard"
+        Me.btnResetFields.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(287, 304)
+        Me.Controls.Add(Me.btnResetFields)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtBody)
+        Me.Controls.Add(Me.txtSubject)
+        Me.Controls.Add(Me.txtTo)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtFrom)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtFrom As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtTo As TextBox
+    Friend WithEvents txtSubject As TextBox
+    Friend WithEvents txtBody As TextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents btnResetFields As Button
+End Class
---
+++ Video_141/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+    Private myMsg As New MailMessage
+    Private smtpSender As New SmtpClient
+
+    Private Sub createMessage()
+        myMsg.From = New MailAddress(txtFrom.Text.Trim)
+        myMsg.To.Add(txtTo.Text.Trim)
+        myMsg.Subject = txtSubject.Text
+        myMsg.Body = txtBody.Text
+        myMsg.Priority = MailPriority.Normal
+    End Sub
+
+    Private Sub createSMTP()
+        smtpSender.Credentials = New Net.NetworkCredential(txtFrom.Text.Trim, txtPassword.Text.Trim)
+        smtpSender.EnableSsl = False
+        smtpSender.Host = "Smtp.live.com"
+        smtpSender.Port = "587"
+    End Sub
+
+    Private Sub sendMessage()
+        Try
+            smtpSender.Send(myMsg)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        createMessage()
+        createSMTP()
+        sendMessage()
+    End Sub
+
+    Private Sub btnResetFields_Click(sender As Object, e As EventArgs) Handles btnResetFields.Click
+        txtBody.Clear()
+        txtFrom.Clear()
+        txtPassword.Clear()
+        txtSubject.Clear()
+        txtTo.Clear()
+    End Sub
+End Class
